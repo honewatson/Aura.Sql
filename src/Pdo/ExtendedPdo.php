24e23290423d7b54a266112c815ed6df3463354c
Paul M. Jones <pmjones88@gmail.com>
2013-07-15 08:09:09 -0500

    reorder methods

--- src/Pdo/ExtendedPdo.php
+++ src/Pdo/ExtendedPdo.php
@@ -93,93 +93,6 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
         }
     }
     
-    protected function beginProfile($function)
-    {
-        // if there's no profiler, can't profile
-        if (! $this->profiler) {
-            return;
-        }
-        
-        // retain starting profile info
-        $this->profile_info['time'] = microtime(true);
-        $this->profile_info['function'] = $function;
-        $this->profile_info['bind_values'] = $this->bind_values;
-    }
-    
-    protected function endProfile(PDOStatement $sth = null)
-    {
-        // if there's no profiler, can't profile
-        if (! $this->profiler) {
-            return;
-        }
-        
-        // add an entry to the profiler
-        $this->profiler->addProfile(
-            microtime(true) - $this->profile_info['time'],
-            $this->profile_info['function'],
-            $sth ? $sth->queryString : null,
-            $this->profile_info['bind_values']
-        );
-        
-        // clear the starting profile info
-        $this->profile_info = [];
-    }
-    
-    /**
-     * 
-     * Connects to the database, begins a transaction, and turns off
-     * autocommit mode.
-     * 
-     * @return bool True on success, false on failure.
-     * 
-     * @see http://php.net/manual/en/pdo.begintransaction.php
-     * 
-     */
-    public function beginTransaction()
-    {
-        $this->connect();
-        $this->beginProfile(__FUNCTION__);
-        $result = parent::beginTransaction();
-        $this->endProfile();
-        return $result;
-    }
-    
-    /**
-     * 
-     * Retains several values to bind to the next query statement; these will
-     * be merges with existing bound values, and will be reset after the
-     * next query.
-     * 
-     * @param array $values An array where the key is the parameter name and
-     * the value is the parameter value.
-     * 
-     * @return void
-     * 
-     */
-    public function bindValues(array $bind_values)
-    {
-        $this->bind_values = array_merge($this->bind_values, $bind_values);
-    }
-    
-    /**
-     * 
-     * Connects to the database, commits the existing transaction, and
-     * restores autocommit mode.
-     * 
-     * @return bool True on success, false on failure.
-     * 
-     * @see http://php.net/manual/en/pdo.commit.php
-     * 
-     */
-    public function commit()
-    {
-        $this->connect();
-        $this->beginProfile(__FUNCTION__);
-        $result = parent::commit();
-        $this->endProfile();
-        return $result;
-    }
-    
     /**
      * 
      * Connects to the database and sets PDO attributes.
@@ -215,6 +128,161 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
         }
     }
     
+    public function isConnected()
+    {
+        return $this->connected;
+    }
+    
+    public function setAttribute($attribute, $value)
+    {
+        if ($this->connected) {
+            return parent::setAttribute($attribute, $value);
+        } else {
+            $this->attributes[$attribute] = $value;
+        }
+    }
+    
+    public function getAttribute($attribute)
+    {
+        $this->connect();
+        return parent::getAttribute($attribute);
+    }
+    
+    public function errorCode()
+    {
+        $this->connect();
+        return parent::errorCode();
+    }
+    
+    public function errorInfo()
+    {
+        $this->connect();
+        return parent::errorInfo();
+    }
+    
+    /**
+     * 
+     * Retains several values to bind to the next query statement; these will
+     * be merges with existing bound values, and will be reset after the
+     * next query.
+     * 
+     * @param array $values An array where the key is the parameter name and
+     * the value is the parameter value.
+     * 
+     * @return void
+     * 
+     */
+    public function bindValues(array $bind_values)
+    {
+        $this->bind_values = array_merge($this->bind_values, $bind_values);
+    }
+    
+    /**
+     * 
+     * Returns the array of values to bind to the next query.
+     * 
+     * @return array
+     * 
+     */
+    public function getBindValues()
+    {
+        return $this->bind_values;
+    }
+    
+    /**
+     * 
+     * Connects to the database and prepares an SQL statement to be executed,
+     * using values that been bound for the next query.
+     * 
+     * This override only binds values that have placeholders in the
+     * statement, thereby avoiding errors from PDO regarding too many bound
+     * values.
+     * 
+     * If a placeholder value is an array, the array is converted to a string
+     * of comma-separated quoted values; e.g., for an `IN (...)` condition.
+     * The quoted string is replaced directly into the statement instead of
+     * using `PDOStatement::bindValue()` proper.
+     * 
+     * @param string $statement The SQL statement to prepare for execution.
+     * 
+     * @param array $options Set these attributes on the returned
+     * PDOStatement.
+     * 
+     * @return PDOStatement
+     * 
+     * @see http://php.net/manual/en/pdo.prepare.php
+     * 
+     */
+    public function prepare($statement, $options = [])
+    {
+        $this->connect();
+        
+        // are there any bind values?
+        if (! $this->bind_values) {
+            return parent::prepare($statement, $options);
+        }
+
+        // a list of placeholders to bind at the end of this method
+        $placeholders = array();
+
+        // find all parts not inside quotes or backslashed-quotes
+        $apos = "'";
+        $quot = '"';
+        $parts = preg_split(
+            "/(($apos+|$quot+|\\$apos+|\\$quot+).*?)\\2/m",
+            $statement,
+            -1,
+            PREG_SPLIT_DELIM_CAPTURE
+        );
+
+        // loop through the non-quoted parts (0, 3, 6, 9, etc.)
+        $k = count($parts);
+        for ($i = 0; $i <= $k; $i += 3) {
+
+            // get the part as a reference so it can be modified in place
+            $part =& $parts[$i];
+
+            // find all :placeholder matches in the part
+            preg_match_all(
+                "/\W:([a-zA-Z_][a-zA-Z0-9_]*)/m",
+                $part . PHP_EOL,
+                $matches
+            );
+
+            // for each of the :placeholder matches ...
+            foreach ($matches[1] as $key) {
+                // is the corresponding data element an array?
+                $bind_array = isset($this->bind_values[$key])
+                           && is_array($this->bind_values[$key]);
+                if ($bind_array) {
+                    // PDO won't bind an array; quote and replace directly
+                    $find = "/(\W)(:$key)(\W)/m";
+                    $repl = '${1}'
+                          . $this->quote($this->bind_values[$key])
+                          . '${3}';
+                    $part = preg_replace($find, $repl, $part);
+                } else {
+                    // not an array, retain the placeholder name for later
+                    $placeholders[] = $key;
+                }
+            }
+        }
+
+        // bring the parts back together in case they were modified
+        $statement = implode('', $parts);
+
+        // prepare the statement
+        $sth = parent::prepare($statement, $options);
+
+        // for the placeholders we found, bind the corresponding data values
+        foreach ($placeholders as $key) {
+            $sth->bindValue($key, $this->bind_values[$key]);
+        }
+
+        // clear out bind values, and done!
+        return $sth;
+    }
+    
     /**
      * 
      * Connects to the database, prepares a statement using the bound values,
@@ -241,25 +309,48 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
     
     /**
      * 
-     * Is a transaction currently active?
+     * Connects to the database, prepares a statement using the bound values,
+     * executes the statement, and returns a PDOStatement result set.
      * 
-     * @return bool
+     * @param string $statement The SQL statement to prepare and execute.
      * 
-     * @see http://php.net/manual/en/pdo.intransaction.php
+     * @param int $fetch_mode The `PDO::FETCH_*` type to set on the returned
+     * `PDOStatement::setFetchMode()`.
      * 
+     * @param mixed $fetch_arg1 The first additional argument to send to
+     * `PDOStatement::setFetchMode()`.
+     * 
+     * @param mixed $fetch_arg2 The second additional argument to send to
+     * `PDOStatement::setFetchMode()`.
+     * 
+     * @return PDOStatement
+     * 
+     * @see http://php.net/manual/en/pdo.query.php
      */
-    public function inTransaction()
-    {
-        $this->connect();
+    public function query(
+        $statement,
+        $fetch_mode = null,
+        $fetch_arg1 = null,
+        $fetch_arg2 = null
+    ) {
+        // prepare and execute
+        $sth = $this->prepare($statement);
         $this->beginProfile(__FUNCTION__);
-        $result = parent::inTransaction();
-        $this->endProfile();
-        return $result;
-    }
-    
-    public function isConnected()
-    {
-        return $this->connected;
+        $sth->execute();
+        $this->endProfile($sth);
+        
+        // allow for optional fetch mode
+        if ($fetch_arg2 !== null) {
+            $sth->setFetchMode($fetch_mode, $fetch_arg1, $fetch_arg2);
+        } elseif ($fetch_arg1 !== null) {
+            $sth->setFetchMode($fetch_mode, $fetch_arg1);
+        } elseif ($fetch_mode !== null) {
+            $sth->setFetchMode($fetch_mode);
+        }
+        
+        // done
+        $this->bind_values = [];
+        return $sth;
     }
     
     /**
@@ -281,18 +372,6 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
         $result = parent::lastInsertId($name);
         $this->endProfile();
         return $result;
-    }
-    
-    public function errorCode()
-    {
-        $this->connect();
-        return parent::errorCode();
-    }
-    
-    public function errorInfo()
-    {
-        $this->connect();
-        return parent::errorInfo();
     }
     
     /**
@@ -415,167 +494,78 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
         return $sth->fetchColumn(0);
     }
 
-    public function getAttribute($attribute)
+    /**
+     * 
+     * Connects to the database, begins a transaction, and turns off
+     * autocommit mode.
+     * 
+     * @return bool True on success, false on failure.
+     * 
+     * @see http://php.net/manual/en/pdo.begintransaction.php
+     * 
+     */
+    public function beginTransaction()
     {
         $this->connect();
-        return parent::getAttribute($attribute);
-    }
-    
-    /**
-     * 
-     * Returns the array of values to bind to the next query.
-     * 
-     * @return array
-     * 
-     */
-    public function getBindValues()
-    {
-        return $this->bind_values;
-    }
-    
-    public function getProfiler()
-    {
-        return $this->profiler;
-    }
-    
-    /**
-     * 
-     * Connects to the database and prepares an SQL statement to be executed,
-     * using values that been bound for the next query.
-     * 
-     * This override only binds values that have placeholders in the
-     * statement, thereby avoiding errors from PDO regarding too many bound
-     * values.
-     * 
-     * If a placeholder value is an array, the array is converted to a string
-     * of comma-separated quoted values; e.g., for an `IN (...)` condition.
-     * The quoted string is replaced directly into the statement instead of
-     * using `PDOStatement::bindValue()` proper.
-     * 
-     * @param string $statement The SQL statement to prepare for execution.
-     * 
-     * @param array $options Set these attributes on the returned
-     * PDOStatement.
-     * 
-     * @return PDOStatement
-     * 
-     * @see http://php.net/manual/en/pdo.prepare.php
-     * 
-     */
-    public function prepare($statement, $options = [])
-    {
-        $this->connect();
-        
-        // are there any bind values?
-        if (! $this->bind_values) {
-            return parent::prepare($statement, $options);
-        }
-
-        // a list of placeholders to bind at the end of this method
-        $placeholders = array();
-
-        // find all parts not inside quotes or backslashed-quotes
-        $apos = "'";
-        $quot = '"';
-        $parts = preg_split(
-            "/(($apos+|$quot+|\\$apos+|\\$quot+).*?)\\2/m",
-            $statement,
-            -1,
-            PREG_SPLIT_DELIM_CAPTURE
-        );
-
-        // loop through the non-quoted parts (0, 3, 6, 9, etc.)
-        $k = count($parts);
-        for ($i = 0; $i <= $k; $i += 3) {
-
-            // get the part as a reference so it can be modified in place
-            $part =& $parts[$i];
-
-            // find all :placeholder matches in the part
-            preg_match_all(
-                "/\W:([a-zA-Z_][a-zA-Z0-9_]*)/m",
-                $part . PHP_EOL,
-                $matches
-            );
-
-            // for each of the :placeholder matches ...
-            foreach ($matches[1] as $key) {
-                // is the corresponding data element an array?
-                $bind_array = isset($this->bind_values[$key])
-                           && is_array($this->bind_values[$key]);
-                if ($bind_array) {
-                    // PDO won't bind an array; quote and replace directly
-                    $find = "/(\W)(:$key)(\W)/m";
-                    $repl = '${1}'
-                          . $this->quote($this->bind_values[$key])
-                          . '${3}';
-                    $part = preg_replace($find, $repl, $part);
-                } else {
-                    // not an array, retain the placeholder name for later
-                    $placeholders[] = $key;
-                }
-            }
-        }
-
-        // bring the parts back together in case they were modified
-        $statement = implode('', $parts);
-
-        // prepare the statement
-        $sth = parent::prepare($statement, $options);
-
-        // for the placeholders we found, bind the corresponding data values
-        foreach ($placeholders as $key) {
-            $sth->bindValue($key, $this->bind_values[$key]);
-        }
-
-        // clear out bind values, and done!
-        return $sth;
-    }
-    
-    /**
-     * 
-     * Connects to the database, prepares a statement using the bound values,
-     * executes the statement, and returns a PDOStatement result set.
-     * 
-     * @param string $statement The SQL statement to prepare and execute.
-     * 
-     * @param int $fetch_mode The `PDO::FETCH_*` type to set on the returned
-     * `PDOStatement::setFetchMode()`.
-     * 
-     * @param mixed $fetch_arg1 The first additional argument to send to
-     * `PDOStatement::setFetchMode()`.
-     * 
-     * @param mixed $fetch_arg2 The second additional argument to send to
-     * `PDOStatement::setFetchMode()`.
-     * 
-     * @return PDOStatement
-     * 
-     * @see http://php.net/manual/en/pdo.query.php
-     */
-    public function query(
-        $statement,
-        $fetch_mode = null,
-        $fetch_arg1 = null,
-        $fetch_arg2 = null
-    ) {
-        // prepare and execute
-        $sth = $this->prepare($statement);
         $this->beginProfile(__FUNCTION__);
-        $sth->execute();
-        $this->endProfile($sth);
-        
-        // allow for optional fetch mode
-        if ($fetch_arg2 !== null) {
-            $sth->setFetchMode($fetch_mode, $fetch_arg1, $fetch_arg2);
-        } elseif ($fetch_arg1 !== null) {
-            $sth->setFetchMode($fetch_mode, $fetch_arg1);
-        } elseif ($fetch_mode !== null) {
-            $sth->setFetchMode($fetch_mode);
-        }
-        
-        // done
-        $this->bind_values = [];
-        return $sth;
+        $result = parent::beginTransaction();
+        $this->endProfile();
+        return $result;
+    }
+    
+    /**
+     * 
+     * Is a transaction currently active?
+     * 
+     * @return bool
+     * 
+     * @see http://php.net/manual/en/pdo.intransaction.php
+     * 
+     */
+    public function inTransaction()
+    {
+        $this->connect();
+        $this->beginProfile(__FUNCTION__);
+        $result = parent::inTransaction();
+        $this->endProfile();
+        return $result;
+    }
+    
+    /**
+     * 
+     * Connects to the database, commits the existing transaction, and
+     * restores autocommit mode.
+     * 
+     * @return bool True on success, false on failure.
+     * 
+     * @see http://php.net/manual/en/pdo.commit.php
+     * 
+     */
+    public function commit()
+    {
+        $this->connect();
+        $this->beginProfile(__FUNCTION__);
+        $result = parent::commit();
+        $this->endProfile();
+        return $result;
+    }
+    
+    /**
+     * 
+     * Connects to the database, rolls back the current transaction, and
+     * restores autocommit mode.
+     * 
+     * @return bool True on success, false on failure.
+     * 
+     * @see http://php.net/manual/en/pdo.rollback.php
+     * 
+     */
+    public function rollBack()
+    {
+        $this->connect();
+        $this->beginProfile(__FUNCTION__);
+        $result = parent::rollBack();
+        $this->endProfile();
     }
     
     /**
@@ -611,35 +601,46 @@ class ExtendedPdo extends PDO implements ExtendedPdoInterface
         return parent::quote($value, $parameter_type);
     }
     
-    /**
-     * 
-     * Connects to the database, rolls back the current transaction, and
-     * restores autocommit mode.
-     * 
-     * @return bool True on success, false on failure.
-     * 
-     * @see http://php.net/manual/en/pdo.rollback.php
-     * 
-     */
-    public function rollBack()
-    {
-        $this->connect();
-        $this->beginProfile(__FUNCTION__);
-        $result = parent::rollBack();
-        $this->endProfile();
-    }
-
-    public function setAttribute($attribute, $value)
-    {
-        if ($this->connected) {
-            return parent::setAttribute($attribute, $value);
-        } else {
-            $this->attributes[$attribute] = $value;
-        }
-    }
-    
     public function setProfiler(ProfilerInterface $profiler)
     {
         $this->profiler = $profiler;
     }
+
+    public function getProfiler()
+    {
+        return $this->profiler;
+    }
+    
+    protected function beginProfile($function)
+    {
+        // if there's no profiler, can't profile
+        if (! $this->profiler) {
+            return;
+        }
+        
+        // retain starting profile info
+        $this->profile_info['time'] = microtime(true);
+        $this->profile_info['function'] = $function;
+        $this->profile_info['bind_values'] = $this->bind_values;
+    }
+    
+    protected function endProfile(PDOStatement $sth = null)
+    {
+        // if there's no profiler, can't profile
+        if (! $this->profiler) {
+            return;
+        }
+        
+        // add an entry to the profiler
+        $this->profiler->addProfile(
+            microtime(true) - $this->profile_info['time'],
+            $this->profile_info['function'],
+            $sth ? $sth->queryString : null,
+            $this->profile_info['bind_values']
+        );
+        
+        // clear the starting profile info
+        $this->profile_info = [];
+    }
+    
 }
