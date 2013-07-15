d32d1583400e65170872fae69aa61fcce20df0f4
Paul M. Jones <pmjones88@gmail.com>
2013-07-15 11:17:28 -0500

    fix test defaults

--- tests/globals.default.php
+++ tests/globals.default.php
@@ -3,31 +3,31 @@
  * Mysql
  */
 $GLOBALS['Aura\Sql\Setup\MysqlSetup']['connection_params'] = [
-    'mysql:host=127.0.0.1',
-    'username' => 'root',
-    'password' => '',
-    'options' => [],
-    'attributes' => [],
+    'dsn'           => 'mysql:host=127.0.0.1',
+    'username'      => 'root',
+    'password'      => '',
+    'options'       => [],
+    'attributes'    => [],
 ];
 
 /**
  * Pgsql
  */
 $GLOBALS['Aura\Sql\Setup\PgsqlSetup']['connection_params'] = [
-    'pgsql:host=127.0.0.1;dbname=test',
-    'username' => 'postgres',
-    'password' => '',
-    'options' => [],
-    'attributes' => [],
+    'dsn'           => 'pgsql:host=127.0.0.1;dbname=test',
+    'username'      => 'postgres',
+    'password'      => '',
+    'options'       => [],
+    'attributes'    => [],
 ];
 
 /**
  * Sqlite
  */
 $GLOBALS['Aura\Sql\Setup\SqliteSetup']['connection_params'] = [
-    'dsn' => 'sqlite::memory:',
-    'username' => null,
-    'password' => null,
-    'options' => [],
-    'attributes' => [],
+    'dsn'           => 'sqlite::memory:',
+    'username'      => null,
+    'password'      => null,
+    'options'       => [],
+    'attributes'    => [],
 ];
